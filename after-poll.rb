cef3c27fe36a1f35399824f1d35af9673d8a9f2c
Gaute Hope <eg@gaute.vetsj.com>
2013-08-30 08:47:26 +0200

    add after-poll

---
+++ after-poll.rb
@@ -0,0 +1,20 @@
+# Send notification to desktop about received emails
+
+notify_cmd="/usr/bin/notify-send -i /usr/share/icons/gnome/24x24/status/stock_mail-unread.png "
+
+if num >= 1
+  notify_summary="Jei! Meil! "
+
+  notify_body = "#{num} "
+  if num > 1
+    notify_body << "nye meila!"
+  else
+    notify_body << "ny meil!"
+  end
+
+  #notify_body = ""
+  #from_and_subj.each { |f,s| notify_body << "\n#{f.dump} : #{s.dump}" }
+  system "#{notify_cmd} '#{notify_summary}' '#{notify_body}'"
+
+  #system 'dbus-send --print-reply --dest=org.freedesktop.Notifications /org/freedesktop/Notifications org.freedesktop.Notifications.Notify string:"Sup" int32:1 string:"/usr/share/icons/gnome/24x24/status/mail-unread.png" string:"' << notify_summary << '" string:"" array:string:"" dict:string:boolean:"transient","true"'
+end
